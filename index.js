0c95787f8d36ff94c7895cc7462ff1f373ad25b0
Tom MacWright <tom@macwright.org>
2015-01-21 11:48:23 -0800

    Fix point form

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ var inside = require('turf-inside');
 *   turf.polygon([[[0,0],[10,0],[10,10],[0,10],[0,0]]]),
 *   turf.polygon([[[10,0],[20,10],[20,20], [20,0]]])]);
 * var points = turf.featurecollection([
-*   turf.point(5,5, {population: 200}),
-*   turf.point(1,3, {population: 600}),
-*   turf.point(14,2, {population: 100}),
-*   turf.point(13,1, {population: 200}),
-*   turf.point(19,7, {population: 300})]);
+*   turf.point([5,5], {population: 200}),
+*   turf.point([1,3], {population: 600}),
+*   turf.point([14,2], {population: 100}),
+*   turf.point([13,1], {population: 200}),
+*   turf.point([19,7], {population: 300})]);
 * var aggregated = turf.median(polygons, points, 'population', 'median');
 * //=polygons
 * //=points
