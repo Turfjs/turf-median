60d85758e0d5e4f6533e08b2052a25278fe13baa
Tom MacWright <tom@macwright.org>
2015-01-21 17:16:06 -0800

    Remove simple-statistics dep

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var ss = require('simple-statistics');
 var inside = require('turf-inside');
 
 /**
@@ -38,8 +37,28 @@ module.exports = function(polyFC, ptFC, inField, outField){
         values.push(pt.properties[inField]);
       }
     });
-    poly.properties[outField] = ss.median(values);
+    poly.properties[outField] = median(values);
   });
 
   return polyFC;
+}
+
+function median(x) {
+    // The median of an empty list is null
+    if (x.length === 0) return null;
+
+    // Sorting the array makes it easy to find the center, but
+    // use `.slice()` to ensure the original array `x` is not modified
+    var sorted = x.slice().sort(function (a, b) { return a - b; });
+
+    // If the length of the list is odd, it's the central number
+    if (sorted.length % 2 === 1) {
+        return sorted[(sorted.length - 1) / 2];
+    // Otherwise, the median is the average of the two numbers
+    // at the center of the list
+    } else {
+        var a = sorted[(sorted.length / 2) - 1];
+        var b = sorted[(sorted.length / 2)];
+        return (a + b) / 2;
+    }
 }
