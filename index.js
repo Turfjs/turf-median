8318f09d915515992cf68995a0a2ca2f82ed75b2
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-26 12:47:18 -0800

    tighten description language

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var inside = require('turf-inside');
 
 /**
- * Calculates the median value of a field for a set of points within a set of polygons.
+ * Calculates the median value of a field for a set of {@link Point|points} within a set of {@link Polygon|polygons}.
  *
  * @module turf/median
  * @category aggregation
